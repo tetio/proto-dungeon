45ee7a9fa30fa78f769454389e6f18d6b64e8e0f
Sergi Maymí <tetio@live.com>
2021-11-01 19:28:35 +0100

    movement is working, no RigidBody

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,114 +1,90 @@
 using System;
 using UnityEngine;
-using System.Linq;
+using System.Collections;
+
 public class Hero : MonoBehaviour
 {
+    int layerMask = ~(1 << 10);
     private float speed = 5f; //0.05f;
-    private float blockV = 0;
-    private float blockH = 0;
-    private float nextValue = 0;
 
-    public GameObject _gameManager;
-    GameManager gameManager;
+    public Vector3 dstPosition;
 
+    private float duration = 0.2f;
 
+    bool free = true;
+    // public GameObject _gameManager;
+    // GameManager gameManager;
+
+Rigidbody2D rb;
     // Start is called before the first frame update
     void Awake()
     {
-        gameManager = _gameManager.GetComponent<GameManager>();//GetComponent<GameManager>();
+        // gameManager = _gameManager.GetComponent<GameManager>();//GetComponent<GameManager>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!free)
+        {
+            return;
+        }
         float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-        // if (horizontal > 0) {
-        //     var pos = new Vector2(transform.position.x+1, transform.position.y);
-        //     if (gameManager.walls.Where(w => w == pos).Count() > 0)
-        //         return; 
-        // } else if (horizontal < 0) {
-        //     var pos = new Vector2(transform.position.x-1, transform.position.y);
-        //     if (gameManager.walls.Where(w => w == pos).Count() > 0)
-        //         return; 
-        // } else if (vertical > 0) {
-        //     var pos = new Vector2(transform.position.x, transform.position.y+1);
-        //     if (gameManager.walls.Where(w => w == pos).Count() > 0)
-        //         return; 
-        // } else if (vertical < 0) {
-        //     var pos = new Vector2(transform.position.x, transform.position.y-1);
-        //     if (gameManager.walls.Where(w => w == pos).Count() > 0)
-        //         return; 
-        // }   
-
-/*
-        if (horizontal != 0 || blockH != 0)
-        { 
-            if (blockH == 0)
-            {
-                blockH = Math.Sign(horizontal) * speed;
-                float truncated = (float)(Math.Truncate((double)transform.position.x * 100.0) / 100.0);
-                nextValue = (horizontal > 0) ? truncated + 1 : truncated - 1;
-                transform.Translate(new Vector3(blockH, 0, 0));
-            }
-            else
-            {
-                if ((blockH > 0 && transform.position.x > nextValue) ||
-                    (blockH < 0 && transform.position.x < nextValue))
-                {
-                    transform.position = new Vector3(nextValue, transform.position.y, 0);
-                    blockH = 0;
-                    nextValue = 0;
-                    Input.ResetInputAxes();
-                }
-                else
-                {
-                    transform.Translate(new Vector3(blockH, 0, 0));
-                }
-            }
-
-        }
-        if (vertical != 0 || blockV != 0)
+        if (vertical > 0)
         {
-            if (blockV == 0)
-            {
-                blockV = Math.Sign(vertical) * speed;
-                float truncated = (float)(Math.Truncate((double)transform.position.y * 100.0) / 100.0);
-                nextValue = (vertical > 0) ? truncated + 1 : truncated - 1;
-                transform.Translate(new Vector3(0, blockV, 0));
-            }
-            else
-            {
-                if ((blockV > 0 && transform.position.y > nextValue) ||
-                    (blockV < 0 && transform.position.y < nextValue))
-                {
-                    transform.position = new Vector3(transform.position.x, nextValue, 0);
-                    blockV = 0;
-                    nextValue = 0;
-                    Input.ResetInputAxes();
-                }
-                else
-                {
-                    transform.Translate(new Vector3(0, blockV, 0));
-                }
-            }
+            dstPosition = transform.position + Vector3.up;
+            if (CheckForWall(dstPosition))
+                StartCoroutine(LerpPosition(dstPosition, duration)); //will do the lerp over two seconds
         }
-        */
-
-        transform.Translate(horizontal, vertical, 0);
+        else if (vertical < 0)
+        {
+            dstPosition = transform.position + Vector3.down;
+            if (CheckForWall(dstPosition))
+                StartCoroutine(LerpPosition(dstPosition, duration)); //will do the lerp over two seconds
+        }
+        else if (horizontal > 0)
+        {
+            dstPosition = transform.position + Vector3.right;
+            if (CheckForWall(dstPosition))
+                StartCoroutine(LerpPosition(dstPosition, duration)); //will do the lerp over two seconds
+        }
+        else if (horizontal < 0)
+        {
+            dstPosition = transform.position + Vector3.left;
+            if (CheckForWall(dstPosition))
+                StartCoroutine(LerpPosition(dstPosition, duration)); //will do the lerp over two seconds
+        }
     }
 
+    private bool CheckForWall(Vector3 position)
+     {
+    //    Collider[] colliders = Physics.OverlapSphere(position, 0.0f);
+    //    return colliders.Length == 0; //returns all the colliders that contain your position
+        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero, 15f, layerMask);
+        if (hit.collider != null && hit.collider.tag == "WALL")
+        {
+            return false; // tHere's a wall
+        }
+        return true; // no wall!
+    }
 
-    void OnTriggerEnter(Collider collider)
+    IEnumerator LerpPosition(Vector3 targetPosition, float duration)
     {
-        // foreach (ContactPoint contact in collision.contacts)
-        // {
-        //     Debug.DrawRay(contact.point, contact.normal, Color.white);
-        // }
+        free = false;
+        float time = 0;
+        Vector3 startPosition = transform.position;
 
-        // if (collision.relativeVelocity.magnitude > 2)
-        //     audioSource.Play();  
-        Debug.Log("HERO: poing!");
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        free = true;
+        Input.ResetInputAxes();
     }
 }
